14773fe5282fceeae65f1b18000902f24e81542c
YYDeemo <466871587@qq.com>
2021-01-13 22:55:54 +0800

    add a txt file to record questions about the project

---
+++ src/syntax/Graph.java
@@ -0,0 +1,17 @@
+package syntax;
+
+public class Graph {
+    /**
+     * this class is a graph which has features as follow:
+     * program
+     * nodes
+     * vectors
+     * function:
+     * initGraph()
+     *
+     *
+     */
+
+
+
+}
